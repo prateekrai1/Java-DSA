34aac8496b0ff022ebe6cfe95fa7f3a4a0b2303a
prateekrai1 <prateek@gmail.com>
2024-07-14 09:32:32 -0400

    Spiral Traversal

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1,5,0,10,14};
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
+       System.out.println(1%10);
+
+
+
    }
 }
